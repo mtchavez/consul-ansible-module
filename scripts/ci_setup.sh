43a2603a59308b1a9c724e274b5b05c2e21f4a21
Chavez <matthew@el-chavez.me>
2015-04-02 08:14:49 -0700

    Move to usr local bin

--- scripts/ci_setup.sh
+++ scripts/ci_setup.sh
@@ -7,11 +7,8 @@ set -ev
 CONSUL_VER=0.5.0
 CONSUL_DL_URL=https://dl.bintray.com/mitchellh/consul/${CONSUL_VER}_$(uname -s)_$(uname -m).zip
 
-curl -L $CONSUL_DL_URL > $PWD/bin/consul
-chmod ugo+x $PWD/bin/consul
-
-export PATH=$PWD/bin:$PATH
-echo $PATH
+curl -L $CONSUL_DL_URL > /usr/local/bin/consul
+chmod ugo+x /usr/local/bin/consul
 
 consul agent --server=true --bootstrap-expect=1 --data-dir=.
 
