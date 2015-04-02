d85b51d9881ff5c8d6151b7f71a6c46544cff749
Chavez <matthew@el-chavez.me>
2015-04-02 07:54:44 -0700

    Debug path

--- scripts/ci_setup.sh
+++ scripts/ci_setup.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
 # Install and set up consul to run server for tests
 #
-
-set -e
-set -m
+set -ev
 
 CONSUL_VER=0.5.0
 CONSUL_DL_URL=https://dl.bintray.com/mitchellh/consul/${CONSUL_VER}_$(uname -s)_$(uname -m).zip
@@ -13,6 +11,7 @@ curl -L $CONSUL_DL_URL > $PWD/bin/consul
 chmod +x $PWD/bin/consul
 
 export PATH=$PATH:$PWD/bin
+echo $PATH
 
 consul agent --server=true --bootstrap-expect=1 --data-dir=.
 
