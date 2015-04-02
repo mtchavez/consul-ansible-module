b74a520609d1f05425b24e748cdb7aa56d621abf
Chavez <matthew@el-chavez.me>
2015-04-02 09:11:30 -0700

    Unzip first

--- scripts/ci_setup.sh
+++ scripts/ci_setup.sh
@@ -7,7 +7,8 @@ set -ev
 CONSUL_VER=0.5.0
 CONSUL_DL_URL=https://dl.bintray.com/mitchellh/consul/${CONSUL_VER}_linux_amd64.zip
 
-curl -L $CONSUL_DL_URL > $PWD/bin/consul
+curl -L $CONSUL_DL_URL > consul.zip
+unzip -o consul.zip -d $PWD/bin/
 chmod +x $PWD/bin/consul
 
 export PATH=$PATH:$PWD/bin
